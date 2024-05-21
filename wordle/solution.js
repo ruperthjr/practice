30bea2fb4edef1f7d8fb0ba589689c4cdd4e51a5
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2024-05-21 11:07:25 +0300

    ft:updtes

---
+++ wordle/solution.js
@@ -0,0 +1,55 @@
+class Wordle {
+    constructor(hiddenWord) {
+      this.hiddenWord = hiddenWord.toLowerCase();
+      this.guessesLeft = 6;
+      this.guesses = new Set();
+    }
+  
+    makeGuess(guess) {
+      guess = guess.toLowerCase();
+      
+      if (this.guesses.has(guess)) {
+        console.log("You already guessed that!");
+        return;
+      }
+  
+      this.guesses.add(guess);
+  
+      let feedback = this.getFeedback(guess);
+  
+      this.guessesLeft--;
+  
+      if (feedback === 'Correct!') {
+        console.log("Congratulations! You guessed the word:", this.hiddenWord);
+      } else if (this.guessesLeft === 0) {
+        console.log("Sorry, you're out of guesses. The word was:", this.hiddenWord);
+      } else {
+        console.log(feedback + ". You have " + this.guessesLeft + " guesses left.");
+      }
+    }
+  
+    getFeedback(guess) {
+      if (guess === this.hiddenWord) {
+        return 'Correct!';
+      }
+  
+      let feedback = '';
+      for (let i = 0; i < this.hiddenWord.length; i++) {
+        if (this.hiddenWord[i] === guess[i]) {
+          feedback += guess[i].toUpperCase();
+        } else if (this.hiddenWord.includes(guess[i])) {
+          feedback += guess[i];
+        } else {
+          feedback += '-';
+        }
+      }
+      return feedback;
+    }
+  }
+  
+  // Example:
+  const wordleGame = new Wordle("apple");
+  wordleGame.makeGuess("apricot");
+  wordleGame.makeGuess("orange");
+  wordleGame.makeGuess("apple");
+  
